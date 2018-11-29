892299a56dc0ab8cb7113c10ee48ce053e89a789
Alejandro <Alejandrehl@icloud.com>
2018-11-29 14:28:27 -0300

    SE agrego firebase

--- Fire.js
+++ Fire.js
@@ -1,4 +1,20 @@
-class Fire {}
+import firebase from 'firebase';
+class Fire {
+    constructor() {
+        this.init();
+        this.observeAuth();
+    }
+
+    init = () =>
+        firebase.initializeApp({
+            apiKey: "AIzaSyCCIh3YpL8NXEh-p7sqYIae1GLumblK0xw",
+            authDomain: "reactnative-chatapp-c2322.firebaseapp.com",
+            databaseURL: "https://reactnative-chatapp-c2322.firebaseio.com",
+            projectId: "reactnative-chatapp-c2322",
+            storageBucket: "reactnative-chatapp-c2322.appspot.com",
+            messagingSenderId: "318133957734"
+        });
+}
 
 Fire.shared = new Fire();
 export default Fire;
