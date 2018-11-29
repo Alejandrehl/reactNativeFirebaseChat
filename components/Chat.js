780c79640ab666fba7131e933d17f7675d25e09c
Alejandro <Alejandrehl@icloud.com>
2018-11-29 14:18:40 -0300

    Configuracion inicial de Chat Component

--- components/Chat.js
+++ components/Chat.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import Fire from '../Fire';
 
+type Props = {
+  name?: string,
+};
 class Chat extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: (navigation.state.params || {}).name || 'Chat!',
+  });
+
+  state = {
+    messages: [],
+  };
+
+  get user() {
+    return {
+      name: this.props.navigation.state.params.name,
+      _id: Fire.shared.uid,
+    };
+  }
+
   render() {
-    return <View />;
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={Fire.shared.send}
+        user={this.user}
+      />
+    );
+  }
+
+  componentDidMount() {
+    Fire.shared.on(message =>
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, message),
+      }))
+    );
+  }
+  componentWillUnmount() {
+    Fire.shared.off();
   }
 }
-
-const styles = StyleSheet.create({});
 
 export default Chat;
